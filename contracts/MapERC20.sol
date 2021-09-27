fd588332c452e7eefe6e15602986e59be7fbe32c
longcpp <longcpp9@gmail.com>
2021-09-27 19:12:14 +0800

    refactor MapERC20 contract

--- contracts/MapERC20.sol
+++ contracts/MapERC20.sol
@@ -1,134 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-// SPDX-License-Identifier: UNLICENSED
-
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract MapERC20 is IERC20 {
-    using SafeERC20 for IERC20;
-    using SafeMath for uint256;
+contract MapERC20 is ERC20Burnable {
+    uint8 immutable private DECIMALS;
 
-    string public name;
-    string public symbol;
-    uint8  public immutable decimals;
-    address public immutable tokenBinding;
-    uint256 private _totalSupply;
+    address public router;
 
-    mapping(address => uint256) public override balanceOf;
-    mapping(address => mapping(address => uint256)) public override allowance;
-
-    mapping(address => bool) public isAuth;
-    address[] public auther;
-
-    modifier onlyAuth() {
-        require(isAuth[msg.sender], "onlyAuth");
+    modifier onlyRouter() {
+        require(msg.sender == router, "forbidden");
         _;
     }
 
-    event LogSwapin(bytes32 indexed txhash, address indexed account, uint amount);
-    event LogSwapout(address indexed account, address indexed bindaddr, uint amount);
-
-    constructor(string memory _name, string memory _symbol, address _tokenBinding) {
-        name = _name;
-        symbol = _symbol;
-        decimals = IERC20Metadata(_tokenBinding).decimals();
-        tokenBinding = _tokenBinding;
-        isAuth[msg.sender] = true;
+    constructor(
+        address token, string memory _name, string memory _symbol, address _router
+    ) ERC20(_name, _symbol) {
+        DECIMALS = IERC20Metadata(token).decimals();
+        router = _router;
     }
 
-    function setAuth(address auth) onlyAuth public {
-        isAuth[auth] = true;
+    function decimals() public view override returns (uint8) {
+        return DECIMALS;
     }
 
-    function removeAuth(address auth) onlyAuth public {
-        isAuth[auth] = false;
-    }
-
-    function totalSupply() external view override returns (uint256) {
-        return _totalSupply;
-    }
-
-    function approve(address spender, uint256 value) external override returns (bool) {
-        allowance[msg.sender][spender] = value;
-        emit Approval(msg.sender, spender, value);
-
-        return true;
-    }
-
-    function transfer(address to, uint256 value) external override returns (bool) {
-        require(to != address(0) || to != address(this));
-        uint256 balance = balanceOf[msg.sender];
-        require(balance >= value, "transfer amount exceeds balance");
-
-        balanceOf[msg.sender] = balance - value;
-        balanceOf[to] += value;
-        emit Transfer(msg.sender, to, value);
-
-        return true;
-    }
-
-    function transferFrom(address from, address to, uint256 value) external override returns (bool) {
-        require(to != address(0) || to != address(this));
-        if (from != msg.sender) {
-            uint256 allowed = allowance[from][msg.sender];
-            if (allowed != type(uint256).max) {
-                require(allowed >= value, "request exceeds allowance");
-                uint256 reduced = allowed - value;
-                allowance[from][msg.sender] = reduced;
-                emit Approval(from, msg.sender, reduced);
-            }
-        }
-
-        uint256 balance = balanceOf[from];
-        require(balance >= value, "transfer amount exceeds balance");
-
-        balanceOf[from] = balance - value;
-        balanceOf[to] += value;
-        emit Transfer(from, to, value);
-
-        return true;
-    }
-
-    function _burn(address account, uint256 amount) internal {
-        require(account != address(0), "burn from the zero address");
-
-        balanceOf[account] -= amount;
-        _totalSupply -= amount;
-        emit Transfer(account, address(0), amount);
-    }
-
-    function _mint(address account, uint256 amount) internal {
-        require(account != address(0), "mint to the zero address");
-
-        _totalSupply += amount;
-        balanceOf[account] += amount;
-        emit Transfer(address(0), account, amount);
-    }
-
-    function mint(address to, uint256 amount) external onlyAuth returns (bool) {
+    function mint(address to, uint256 amount) external onlyRouter {
         _mint(to, amount);
-        return true;
     }
 
-    function burn(address from, uint256 amount) external onlyAuth returns (bool) {
-        require(from != address(0), "burn from the zero address");
-        _burn(from, amount);
-        return true;
-    }
-
-    function Swapin(bytes32 txhash, address account, uint256 amount) public onlyAuth returns (bool) {
-        _mint(account, amount);
-        emit LogSwapin(txhash, account, amount);
-        return true;
-    }
-
-    function Swapout(uint256 amount, address bindaddr) public returns (bool) {
-        require(bindaddr != address(0), "address(0x0)");
-        _burn(msg.sender, amount);
-        emit LogSwapout(msg.sender, bindaddr, amount);
-        return true;
+    function burn(address from, uint256 amount) external onlyRouter {
+        burnFrom(from, amount);
     }
 }
