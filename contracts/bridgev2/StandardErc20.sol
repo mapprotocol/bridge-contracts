8a42d089f427088903ae21a83760c5169e80d57a
jsonfelix <wbqlove1234@gmail.com>
2021-12-20 16:43:48 +0800

    add token manager

--- contracts/bridgev2/StandardErc20.sol
+++ contracts/bridgev2/StandardErc20.sol
@@ -3,17 +3,35 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
+contract Role is AccessControl{
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
-contract Token is ERC20, Ownable {
+    constructor(){
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MANAGER_ROLE, msg.sender);
+    }
+
+    modifier onlyManager(){
+        require(hasRole(MANAGER_ROLE, msg.sender), "Caller is not a manager");
+        _;
+    }
+
+    function addManager(address manager) external onlyRole(DEFAULT_ADMIN_ROLE){
+        _setupRole(MANAGER_ROLE, manager);
+    }
+}
+
+contract Token is ERC20, Role {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     }
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amount) external onlyManager {
         _mint(to, amount);
     }
 
-    function burn(address from, uint256 amount) external onlyOwner {
+    function burn(address from, uint256 amount) external onlyManager {
         _burn(from, amount);
     }
 }
